99af2e3c4c50048fbcdee36e5f48fbac6d686a60
Leonardo Alves <leonardoalves201510@gmail.com>
2024-02-29 07:26:14 -0300

    feat: add welcome message to home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,21 @@
+import { format } from "date-fns";
 import Header from "../_components/header";
+import { Locale } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
 export default function Home() {
   return (
     <div>
       <Header />
+
+      <div className="pt-5 px-5">
+        <h2 className="text-xl font-bold">Olá, Miguel</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR,
+          })}
+        </p>
+      </div>
     </div>
   );
 }
