03ef24cbcee9c441e0c718177ab4887d5c56dc3c
Leonardo Alves <leonardoalves201510@gmail.com>
2024-02-29 07:16:58 -0300

    feat: add header component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,9 @@
-import Image from "next/image";
+import Header from "../_components/header";
 
 export default function Home() {
-  return <h1>fsw</h1>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,5 +1,21 @@
+import { Card, CardContent } from "./ui/card";
+import Image from "next/image";
+import { Button } from "./ui/button";
+import { MenuIcon } from "lucide-react";
+
 const Header = () => {
-  return <h1>Header </h1>;
+  return (
+    <h1>
+      <Card>
+        <CardContent className="p-5 justify-between flex flex-row items-center">
+          <Image src="/logo.png" alt="logo" height={22} width={120} />
+          <Button variant="outline" size="icon" className="h-8 w-8">
+            <MenuIcon size={16} />
+          </Button>
+        </CardContent>
+      </Card>
+    </h1>
+  );
 };
 
 export default Header;
