29a6d4f260df5d85f3c635d504de0f678e7d67a6
Leonardo Alves <leonardoalves201510@gmail.com>
2024-03-20 20:49:17 -0300

    chore: rending ServiceItem map with all services

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,7 +1,6 @@
-import { Button } from "@/app/_components/ui/button";
 import { db } from "@/app/_lib/prisma";
-import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
-import Image from "next/image";
+import BarberShopInfo from "./_components/barbershop-info";
+import ServiceItem from "./_components/service-item";
 
 interface BarberShopDetailsPage {
   params: {
@@ -18,6 +17,9 @@ const BarberShopDetailsPage = async ({ params }: BarberShopDetailsPage) => {
     where: {
       id: params.id,
     },
+    include: {
+      services: true,
+    },
   });
 
   if (!barbershop) {
@@ -26,43 +28,11 @@ const BarberShopDetailsPage = async ({ params }: BarberShopDetailsPage) => {
 
   return (
     <div>
-      <div className="h-[250px] w-full relative">
-        <Button
-          size="icon"
-          variant="outline"
-          className="z-50 absolute top-4 left-4"
-        >
-          <ChevronLeftIcon />
-        </Button>
+      <BarberShopInfo barbershop={barbershop} />
 
-        <Button
-          size="icon"
-          variant="outline"
-          className="z-50 absolute top-4 right-4"
-        >
-          <MenuIcon />
-        </Button>
-
-        <Image
-          src={barbershop.imageUrl}
-          alt="imagem da barbearia"
-          fill
-          style={{ objectFit: "cover" }}
-          className="opacity-85"
-        />
-      </div>
-
-      <div className="px-5 pt-3 pb-6 border-b border-solid border-secondary">
-        <h1 className="text-xl font-bold">{barbershop.name}</h1>
-        <div className="flex items-center gap-1 mt-2">
-          <MapPinIcon className="text-primary" size="18" />
-          <p className="text-sm">{barbershop.address}</p>
-        </div>
-        <div className="flex items-center gap-1 mt-2">
-          <StarIcon className="text-primary" size="18" />
-          <p className="text-sm">5.0 (127 avaliações)</p>
-        </div>
-      </div>
+      {barbershop.services.map((service) => (
+        <ServiceItem key={service.id} service={service} />
+      ))}
     </div>
   );
 };
