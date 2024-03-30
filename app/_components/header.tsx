a5e49e30cab1b661c76b92ab26499926b6bbe2f9
unknown <leonardoalves201510@gmail.com>
2024-03-29 23:30:59 -0300

    .

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -2,16 +2,37 @@ import { Card, CardContent } from "./ui/card";
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { signOut, useSession } from "next-auth/react";
 
 const Header = () => {
+  // const { data, status } = useSession();
+
+  const handleLogoutClick = () => signOut;
   return (
     <h1>
       <Card>
         <CardContent className="p-5 justify-between flex flex-row items-center">
           <Image src="/logo.png" alt="logo" height={22} width={120} />
-          <Button variant="outline" size="icon" className="h-8 w-8">
-            <MenuIcon size={16} />
-          </Button>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="outline" size="icon">
+                <MenuIcon size={16} />
+              </Button>
+            </SheetTrigger>
+
+            <SheetContent className="p-0">
+              <SheetHeader className="text-left border-b border-secondary p-5">
+                <SheetTitle>Menu</SheetTitle>
+              </SheetHeader>
+            </SheetContent>
+          </Sheet>
         </CardContent>
       </Card>
     </h1>
