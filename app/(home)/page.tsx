f6351f10379cfe49cf6cda95e2b576d41a7fe246
Leonardo Alves de Almeida <leonardoalves201510@gmail.com>
2024-04-18 21:07:31 -0300

    feat: showing confirmed Bookings in homepage

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,17 +5,36 @@ import Search from "./_components/search";
 import BookingItem from "../_components/booking-item";
 import { db } from "../_lib/prisma";
 import BarbershopItem from "./_components/barbershop-item";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
 
 export default async function Home() {
-  //CHAMAR PRISMA E CRIAR AS BARBEARIAS
-  const barbershops = await db.barbershop.findMany({});
+  const session = await getServerSession(authOptions);
+
+  const [barbershops, confirmedBookings] = await Promise.all([
+    await db.barbershop.findMany({}),
+    session?.user
+      ? await db.booking.findMany({
+          where: {
+            userId: (session.user as any).id,
+            date: {
+              gte: new Date(),
+            },
+          },
+          include: {
+            service: true,
+            barbershop: true,
+          },
+        })
+      : Promise.resolve([]),
+  ]);
 
   return (
     <div>
       <Header />
 
       <div className="pt-5 px-5">
-        <h2 className="text-xl font-bold">Olá, Miguel</h2>
+        <h2 className="text-xl font-bold">Olá, {session?.user?.name}</h2>
         <p className="capitalize text-sm">
           {format(new Date(), "EEEE',' dd 'de' MMMM", {
             locale: ptBR,
@@ -27,11 +46,15 @@ export default async function Home() {
         <Search />
       </div>
 
-      <div className="px-5 mt-6">
-        <h2 className="text-xs uppercase text-gray-400 font-bold mb-3">
+      <div className="mt-6">
+        <h2 className="pl-5 text-xs uppercase text-gray-400 font-bold mb-3">
           Agendamentos
         </h2>
-        <BookingItem />
+        <div className="px-5 flex gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {confirmedBookings.map((booking) => (
+            <BookingItem key={booking.id} booking={booking} />
+          ))}
+        </div>
       </div>
 
       <div className="mt-6">
